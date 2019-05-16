7d5bcebc07a2f4f9e20318769ecba48cabdaf3c2
CesarRivasP <cesarjose58@gmail.com>
2019-05-16 16:58:47 -0400

    Create and implemented header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import classNames from 'classnames';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  bar: {
+    background: '#60a3bc' ,
+    color: 'white',
+    padding: 0
+  }
+};
+
+const Header = (props) => {
+  const { classes, className, ...other } = props;
+  const { title } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classNames(classes.bar, className)} {...other}>
+        <Toolbar>
+          <h2>{title}</h2>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);

2db1ad578968dda4136717109b8a44b67dc80a41
CesarRivasP <cesarjose58@gmail.com>
2019-05-17 18:39:20 -0400

    IMplemented result component

--- src/components/result.js
+++ src/components/result.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Result extends Component {
   render(){
+    const { result } = this.props;
     return (
-
+      <div className="gran-total">
+        <TransitionGroup component="span" className="result">
+          <CSSTransition className="result" key={result} timeout={{ enter: 500, exit: 500 }}>
+            <span><strong>{ result }</strong></span>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
     )
   }
 }

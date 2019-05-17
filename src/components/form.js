c09f1b744f9465699e6281e2499a269e2fee7948
CesarRivasP <cesarjose58@gmail.com>
2019-05-17 11:09:04 -0400

    Implemented condition for object nulls and send of props for app component

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -11,7 +11,6 @@ import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
-import RootRef from '@material-ui/core/RootRef';
 
 
 const cars = [
@@ -20,7 +19,7 @@ const cars = [
   "Toyota",
 ];
 
-const styles = theme => ({
+const styles = {
   formControlSelect1: {
     marginTop: 25,
     display: 'flex',
@@ -53,16 +52,17 @@ const styles = theme => ({
     display: 'flex',
     margin: 'auto'
   }
-});
+};
 
 class Form extends Component {
   constructor (props){
     super(props);
     this.state = {
       brand: '',
-      value: '',
+      age: '',
+      plan: ''
     }
-    this.brandRef = React.createRef();
+    this.brandRef = React.createRef();  //mediante estas referencias se puede leer cualquier campo o input
     this.ageRef = React.createRef()
     this.planRef = React.createRef()
   }
@@ -71,14 +71,35 @@ class Form extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log('Send!!!');
-    console.log(this.brandRef.props.value);
-    console.log(this.ageRef.props.value);
-    console.log(this.planRef.props.value);
     // obtener los datos
+    // console.log(this.brandRef.props.value);
+    // console.log(this.ageRef.props.value);
+    // console.log(this.planRef.props.value);
+    if(!this.brandRef.props.value){
+      console.log('Debes ingresar la marca de tu vehiculo');
+    }
+    else if(!this.ageRef.props.value){
+      console.log('Debes ingresar el año de tu vehiculo');
+    }
+    else if(!this.planRef.props.value){
+      console.log('Debes ingresar el plan que quieres seleccionar');
+    }
+    else {
+      // crear el objeto
+      const carInfo = {
+        brand: this.brandRef.props.value,
+        age: this.ageRef.props.value,
+        plan: this.planRef.props.value
+      }
+      // console.log(carInfo);
+      // enviarlo al component principal
+      this.props.quoteSafe(carInfo);
+    }
 
-    // crear el objeto
-
-    // enviarlo al component principal
+    // Resetear el formulario
+    this.setState({
+      brand: '', age: ''
+    })
   }
 
   handleChange = (name) => (event) => {
@@ -87,8 +108,9 @@ class Form extends Component {
     this.setState({ [name]: event.target.value });
   };
 
-  handleRadioButtonChange = event => {
-    this.setState({ value: event.target.value });
+  handleRadioButtonChange = (name) => (event) => {
+    console.log(this.planRef.props.value);
+    this.setState({ value: event.target.value});
   };
 
 
@@ -105,7 +127,6 @@ class Form extends Component {
         <h4 className="title-header">Ingrese los datos su vehiculo</h4>
         <div style={{ margin: '30px'}}>
           <div>
-
             <FormControl className={classes.formControlSelect1}>
               <InputLabel htmlFor="brand-native-simple">Marca</InputLabel>
               <Select
@@ -143,11 +164,11 @@ class Form extends Component {
               <FormLabel component="legend">Plan:</FormLabel>
               <RadioGroup
                 ref={(option) => { this.planRef = option }}
-                aria-label="gender"
+                aria-label="planes"
                 name="plans"
                 className={classes.group}
                 value={this.state.value}
-                onChange={this.handleRadioButtonChange}
+                onChange={this.handleRadioButtonChange('plans')}
                 >
                   <FormControlLabel
                     value="basic"

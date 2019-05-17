cf65dea146ffff8df4a767c0779f4f20829ecf1a
CesarRivasP <cesarjose58@gmail.com>
2019-05-17 18:39:38 -0400

    IMpemented result component

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Divider from '@material-ui/core/Divider';
 import { firstMayus } from '../helpers/helpers';
+import Result from './result';
 
 
 class Summary extends Component {
   render(){
     const { brand, age, plan} = this.props.data;
     return (
-      <div className="summary">
-        <Divider variant="middle" style={{ marginBottom: 20, height: 2}}/>
-        <h2>Resumen de cotización</h2>
-        <li>Marca: <strong>{ firstMayus(brand) }</strong></li>
-        <li>Plan: <strong>{ firstMayus(plan) }</strong></li>
-        <li>Año del auto: <strong>{ firstMayus(age) }</strong></li>
+      <div>
+        <div className="summary">
+          <Divider variant="middle" style={{ marginBottom: 20, height: 2}}/>
+          <h2>Resumen de cotización</h2>
+          <li>Marca: <strong>{ firstMayus(brand) }</strong></li>
+          <li>Plan: <strong>{ firstMayus(plan) }</strong></li>
+          <li>Año del auto: <strong>{ firstMayus(age) }</strong></li>
+        </div>
+        <Result result={this.props.result}/>
       </div>
     )
   }

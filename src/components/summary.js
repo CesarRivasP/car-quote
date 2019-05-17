6fec7c8bd2208c6d9cb1988909cb4ff499b0f621
CesarRivasP <cesarjose58@gmail.com>
2019-05-17 16:32:11 -0400

    Implemented information of the component

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Divider from '@material-ui/core/Divider';
+import { firstMayus } from '../helpers/helpers';
+
 
 class Summary extends Component {
   render(){
+    const { brand, age, plan} = this.props.data;
     return (
-
+      <div className="summary">
+        <Divider variant="middle" style={{ marginBottom: 20, height: 2}}/>
+        <h2>Resumen de cotización</h2>
+        <li>Marca: <strong>{ firstMayus(brand) }</strong></li>
+        <li>Plan: <strong>{ firstMayus(plan) }</strong></li>
+        <li>Año del auto: <strong>{ firstMayus(age) }</strong></li>
+      </div>
     )
   }
 }
